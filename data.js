e3790dc9f370d83c0438628840ca10f08f44bbe8
aguiarj1 <aguiarj1@seattleu.edu>
2019-02-16 16:38:31 -0800

    latest changes

--- data.js
+++ data.js
@@ -1,4 +1,9 @@
-
+document.getElementById("listOfClasses").innerHTML = "hello";
 var classes = ['class1', 'class2','class3', 'class4',
               'class5', 'class6','class7', 'class8'];
-alert(classes[1]);
+classes.forEach(function(string){
+    document.getElementById("listOfClasses").innerHTML += " " +string;
+});
+
+
+                
