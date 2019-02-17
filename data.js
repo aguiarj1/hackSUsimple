5fd1357918776c542266636fba53d2893168b5b0
VeeVee <veeveew@gmail.com>
2019-02-16 16:44:30 -0800

    added class test data

--- data.js
+++ data.js
@@ -1,4 +1,91 @@
+var c0 = {
+  term: "summer",
+  section_name: "5300-01",
+  title: "",
+  monday: "",
+  tuesday: "",
+  wednesday: "",
+  thursday: "",
+  friday: "",
+  start_minute: "",
+  end_minute: "",
+  faculty: "",
+  credits: "",
+  PReq: ""
+};
 
-var classes = ['class1', 'class2','class3', 'class4',
-              'class5', 'class6','class7', 'class8'];
+var c1 = {
+  term: "spring",
+  section_name: "5042-01",
+  title: "",
+  monday: "",
+  tuesday: "",
+  wednesday: "",
+  thursday: "",
+  friday: "",
+  start_minute: "",
+  end_minute: "",
+  faculty: "",
+  credits: "",
+  PReq: ""
+};
+
+var c2 = {
+  term: "spring",
+  section_name: "5042-02",
+  title: "",
+  monday: "",
+  tuesday: "",
+  wednesday: "",
+  thursday: "",
+  friday: "",
+  start_minute: "",
+  end_minute: "",
+  faculty: "",
+  credits: "",
+  PReq: ""
+};
+
+var c3 = {
+  term: "spring",
+  section_name: "5042-02",
+  title: "",
+  monday: "",
+  tuesday: "",
+  wednesday: "",
+  thursday: "",
+  friday: "",
+  start_minute: "",
+  end_minute: "",
+  faculty: "",
+  credits: "",
+  PReq: ""
+};
+
+var c4 = {
+  term: "spring",
+  section_name: "",
+  title: "",
+  monday: "",
+  tuesday: "",
+  wednesday: "",
+  thursday: "",
+  friday: "",
+  start_minute: "",
+  end_minute: "",
+  faculty: "",
+  credits: "",
+  PReq: ""
+};
+
+var classes = [
+  "class1",
+  "class2",
+  "class3",
+  "class4",
+  "class5",
+  "class6",
+  "class7",
+  "class8"
+];
 alert(classes[1]);
